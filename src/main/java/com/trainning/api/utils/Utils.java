0d0b14c25ca0f10ad8503e5ef06e89eda35094e8
NguyenVanTien <nguyenvantien@luvina.net>
2019-10-18 14:15:03 +0700

    Delete function not user

--- src/main/java/com/trainning/api/utils/Utils.java
+++ src/main/java/com/trainning/api/utils/Utils.java
@@ -5,27 +5,6 @@ public class Utils {
      * Address utils
      */
     public static class AddressUtils {
-
-        /**
-         * Check null object AddressPrefectureCode
-         * @param object
-         */
-        public static void checkAddressPrefectureCode(Object object){
-            if(object == null){
-                throw new NullPointerException("Prefecture code not found.");
-            }
-        }
-
-        /**
-         * Check null code
-         * @param code code
-         */
-        public static void checkNullCode(String code) {
-            if(code == null || code.isEmpty()) {
-                throw new NullPointerException("Code must be not null.");
-            }
-        }
-
         /**
          * Check number code
          * @param code code
